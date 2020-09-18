958b93e06eefdacaeda7507750a5b2c2de63f1a8
Felix Breidenstein <felix@codemonauts.com>
2020-09-18 12:15:46 +0200

    Better ignore and renamed project
    With the new name and the variables for "{{ project_name }}" in all
    files this repo can now be used with the "--template" feature of
    startproject

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,4 @@
-"""mynewproject URL Configuration
+"""project_name URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('healthz', include("mynewproject.healthz.urls")),
-    path("", include("mynewproject.core.urls")),
+    path('healthz', include("project_name.healthz.urls")),
+    path("", include("project_name.core.urls")),
 ]
